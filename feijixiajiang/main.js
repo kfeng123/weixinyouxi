78a07f9e5cbfa639db7b0d1b13f861ffeffb9860
kfeng123 <446100240@qq.com>
2014-08-12 21:14:03 +0800

    hh

--- feijixiajiang/main.js
+++ feijixiajiang/main.js
@@ -2,27 +2,31 @@ var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'game', { preload: preload, 
 
 function preload() {
     game.load.image('arrow', 'arrow.png');
+	game.load.image('pipe','platform.png');
 }
 
 var plane;
 var Target;
 var cursors;
+var pipe;
+var h;
 
 function create() {
-
+	game.world.setBounds(0, 0, 800, 100000);
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     game.stage.backgroundColor = '#0072bc';
 
-    plane = game.add.sprite(400, 300, 'arrow');
+    plane = game.add.sprite(200, 300, 'arrow');
+	plane.scale.setTo(0.7,0.7);
     plane.anchor.setTo(0.5, 0.5);
 	
-	Target=game.add.sprite(400,500,'arrow');
+	Target=game.add.sprite(1500,1000,'arrow');
 	Target.anchor.setTo(0.5,0.5);
 
     //	Enable Arcade Physics for the sprite
     game.physics.enable(plane, Phaser.Physics.ARCADE);
-
+	plane.body.collideWorldBounds = true;
 	game.physics.enable(Target,Phaser.Physics.ARCADE);
 	
     //	Tell it we don't want physics to manage the rotation
@@ -30,23 +34,52 @@ function create() {
 	
 	cursors = game.input.keyboard.createCursorKeys();
 	game.camera.follow(plane);
-
+	
+	pipe=game.add.group();
+	pipe.enableBody=true;
+	pipe.physicsBodyType = Phaser.Physics.ARCADE;
+	h=plane.y;
+	for (var i=0;i<10;i++){
+		h+=Math.random()*100+200;
+		var temp;
+		temp=pipe.create(i%2*400,h,'pipe');
+		temp.body.immovable=true;
+		
+	}
+	
 }
 
-function update() {
 
-    plane.rotation = game.physics.arcade.moveToObject(plane,Target,60, 1000);
-	//Target.y=plane.y+200;
+
+function update() {
+	game.physics.arcade.overlap(plane,pipe,gameover,null,this);
+	game.physics.arcade.collide(plane, pipe);
+    plane.rotation = game.physics.arcade.moveToObject(plane,Target,60, 5000);
+	
+	Target.y=plane.y+700;
+	
 	if(cursors.right.isDown){
-		Target.body.velocity.x=300;
+		Target.body.velocity.x=1500;
 	}
 	else if(cursors.left.isDown){
-		Target.body.velocity.x=-300;
+		Target.body.velocity.x=-1500;
 	}
 	else{
 		Target.body.velocity.x=0;
 	}
+	pipe.forEach(function(obj){
+		if(obj.y<plane.y-500){
+			h+=Math.random()*100+200;
+			obj.y=h;
+		}
+	});
+	
+}
 
+function gameover(){
+	plane.kill();
+	stateText=game.add.text(game.camera.x+game.camera.width/2,game.camera.y+game.camera.height/2,'game over', { font: '84px Arial', fill: '#fff' });
+	stateText.anchor.setTo(0.5, 0.5);
 }
 
 function render() {
